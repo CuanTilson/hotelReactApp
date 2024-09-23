55aa35496aab31c09c747db57c59a63e98752309
Cúán Tilson <cuantilson@gmail.com>
2024-09-23 19:25:14 +0100

    Add files via upload

---
+++ hotel-app/functions/getHotels.js
@@ -0,0 +1,40 @@
+exports.handler = async function () {
+    const fetch = await import('node-fetch').then(mod => mod.default);
+
+    const query = `
+    query {
+      hotel_data {
+        values {
+          id,
+          name,
+          rating
+        }
+      }
+    }
+    `
+
+    const url = process.env.ENDPOINT
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            "x-cassandra-token": process.env.ASTRA_TOKEN
+        },
+        body: JSON.stringify({query}),
+    }
+    const response = await fetch(url, options)
+
+    try {
+        const responseBody = await response.json()
+        return {
+            statusCode: 200,
+            body: JSON.stringify(responseBody)
+        }
+    } catch (e) {
+        console.log(e)
+        return {
+            statusCode: 500,
+            body: JSON.stringify({e})
+        }
+    }
+}
